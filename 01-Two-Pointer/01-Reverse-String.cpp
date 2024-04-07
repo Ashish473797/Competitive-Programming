2b94aac00a53de6ccfbd2bfd00020b45b4c22dac
Ashish473797 <ashish473797@gmail.com>
2024-04-07 23:38:25 +0530

    updated

--- 01-Two-Pointer/01-Reverse-String.cpp
+++ 01-Two-Pointer/01-Reverse-String.cpp
@@ -3,3 +3,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size()-1;
+        int mid = s.size()/2;
+        while(i < mid){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
